118f1604846b6aa10fdcaf71702b78ad36b45279
Matt Rosno <matt@broftware.com>
2013-06-30 11:48:39 -0500

    More mobile

--- js/javascript.js
+++ js/javascript.js
@@ -22,22 +22,22 @@ $(function() {
 		$(".header-navList > li > a").removeClass("isSelected");
 		if (scrollTop > $("#registry").offset().top - 10) {
 			$("#navRegistry").addClass("isSelected");
-			//$("#navSelect").val("#registry").attr("selected", true);
+			$("#navSelect").val("#registry").attr("selected", true);
 		} else if (scrollTop > $("#lodging").offset().top - 10) {
 			$("#navLodging").addClass("isSelected");
-			//$("#navSelect").val("#lodging").attr("selected", true);
+			$("#navSelect").val("#lodging").attr("selected", true);
 		} else if (scrollTop > $("#events").offset().top - 10) {
 			$("#navEvents").addClass("isSelected");
-			//$("#navSelect").val("#events").attr("selected", true);
+			$("#navSelect").val("#events").attr("selected", true);
 		} else if (scrollTop > $("#family").offset().top - 10) {
 			$("#navFamily").addClass("isSelected");
-			//$("#navSelect").val("#family").attr("selected", true);
+			$("#navSelect").val("#family").attr("selected", true);
 		} else if (scrollTop > $("#weddingParty").offset().top - 10) {
 			$("#navWeddingParty").addClass("isSelected");
-			//$("#navSelect").val("#weddingParty").attr("selected", true);
+			$("#navSelect").val("#weddingParty").attr("selected", true);
 		} else if (scrollTop > $("#story").offset().top - 10) {
 			$("#navStory").addClass("isSelected");
-			//$("#navSelect").val("#story").attr("selected", true);
+			$("#navSelect").val("#story").attr("selected", true);
 		}
 		
 	});
