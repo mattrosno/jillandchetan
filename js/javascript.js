d2b8c6f16feb8a0e5cc1890f6d1f44bf3fbd9c63
Matt Rosno <matt@broftware.com>
2013-07-07 17:17:09 -0500

    Cleanup

--- js/javascript.js
+++ js/javascript.js
@@ -20,39 +20,36 @@ $(function() {
 		}
 		
 		$(".header-navList > li > a").removeClass("isSelected");
-		if (scrollTop > $("#registry").offset().top - 10) {
+		if (scrollTop > $("#registry").offset().top - 26) {
 			$("#navRegistry").addClass("isSelected");
 			$("#navSelect").val("#registry").attr("selected", true);
-		} else if (scrollTop > $("#lodging").offset().top - 10) {
+		} else if (scrollTop > $("#lodging").offset().top - 26) {
 			$("#navLodging").addClass("isSelected");
 			$("#navSelect").val("#lodging").attr("selected", true);
-		} else if (scrollTop > $("#events").offset().top - 10) {
+		} else if (scrollTop > $("#events").offset().top - 26) {
 			$("#navEvents").addClass("isSelected");
 			$("#navSelect").val("#events").attr("selected", true);
-		} else if (scrollTop > $("#family").offset().top - 10) {
+		} else if (scrollTop > $("#family").offset().top - 26) {
 			$("#navFamily").addClass("isSelected");
 			$("#navSelect").val("#family").attr("selected", true);
-		} else if (scrollTop > $("#weddingParty").offset().top - 10) {
+		} else if (scrollTop > $("#weddingParty").offset().top - 26) {
 			$("#navWeddingParty").addClass("isSelected");
 			$("#navSelect").val("#weddingParty").attr("selected", true);
-		} else if (scrollTop > $("#story").offset().top - 10) {
+		} else if (scrollTop > $("#story").offset().top - 26) {
 			$("#navStory").addClass("isSelected");
 			$("#navSelect").val("#story").attr("selected", true);
 		}
-		
 	});
 	
 	$(".header-navList > li > a").click(function() {
-		//$(".header-navList > li > a").removeClass("isSelected");
-		//$(this).addClass("isSelected");
 		$("html, body").animate({
-			scrollTop: $($(this).attr("go")).offset().top,
+			scrollTop: $($(this).attr("go")).offset().top - 25,
 			easing: "easeInCubic"
 		}, 500);
 	});
 	$("#navSelect").change(function() {
 		$("html, body").animate({
-			scrollTop: $($(this).val()).offset().top,
+			scrollTop: $($(this).val()).offset().top - 25,
 			easing: "easeInCubic"
 		}, 500);
 	});
